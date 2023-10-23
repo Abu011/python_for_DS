528d0fba5463fb6610b2729fecd86173421adc8a
abu <abu@gmail.com>
2023-10-23 12:46:34 +0530

    yor first line of code

--- 01_first_pgm.py
+++ 01_first_pgm.py
@@ -1,11 +1,6 @@
+# congratulations you have written your first line of code
 print(5)
 print("Hello world")
 print("python_with_Abu")
 
-import seaborn as sns
-import matplotlib.pyplot as plt
-sns.set_theme(style="ticks", color_codes=True)
 
-titanic = sns.load_dataset("titanic")
-sns.catplot(x="sex", y="survived", hue="class", kind="bar", data=titanic)
-plt.show()
